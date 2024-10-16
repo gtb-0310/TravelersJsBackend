bdacd161e70692b3add4769f511e52c40427e198
Geoffrey Toubeau <geoffrey.toubeau@soprasteria.com>
2024-10-16 09:30:26 +0200

    ajout du module bcryptjs

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const messages = require('../utils/messages');
 
+
 const connectToDB = async() => {
     try {
         await mongoose.connect(process.env.DATABASE_URL)
